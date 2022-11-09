e9e8af0e8273f0ac7a05bdb0db7938d9df71502c
Tristan Cole <93275422+TCole79@users.noreply.github.com>
2022-11-09 13:27:13 -0700

    Update index.js
    * Move comment content to new CommentDetail prop
    * Add author, timeAgo, text, and avatar prop info

--- src/index.js
+++ src/index.js
@@ -1,24 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
+import CommentDetail from './CommentDetail';
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" src={faker.image.image()}/>
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        Frodo
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Today at 6:00PM</span>
-                    </div>
-                    <div className="text">Nice blog post!</div>
-                </div>
-            </div>
+            <CommentDetail 
+            author="Tommy" 
+            timeAgo="Today at 16:45PM" 
+            text="Why Lisa, why?" 
+            avatar={faker.image.image()}/>
+
+            <CommentDetail 
+            author="Tommy W" 
+            timeAgo="Today at 02:00AM" 
+            text="Oh hai Mark!" 
+            avatar={faker.image.image()}/>
+
+            <CommentDetail 
+            author="Wisseau" 
+            timeAgo="Yesterday at 11:15PM" 
+            text="You must be kidding me aren't you." 
+            avatar={faker.image.image()}/>
         </div>
     )
 };
